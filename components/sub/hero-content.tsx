37630d827c1a83b3ad5b2dc12dc9b5434efd26fc
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-06-20 11:52:02 +0530

    optimized

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -49,7 +49,7 @@ const HeroContent = () => {
           <Input id="email" placeholder="Ask Any Question" className="bg-zinc-200 rounded-l-xl p-7 mx-auto " />
           <a className=" relative inline-flex items-center rounded-r-xl justify-center p-4 px-6 py-3 h-14 w-10 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 shadow-md group">
             <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-              <svg className="w-6 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+              <svg className="w-6 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
             </span>
             <span className="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease"></span>
             
