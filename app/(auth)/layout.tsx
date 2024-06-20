3ebcffe576dc2a2227a0b786d3e99b25238c1aa7
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2024-06-20 11:48:35 +0530

    Created Landing Page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+export default function AuthLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <div className="flex items-center justify-center h-screen " >
+        
+
+        {children}
+         
+      </div>
+    );
+  }
+  
